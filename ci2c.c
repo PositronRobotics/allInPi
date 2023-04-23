cc257a61290db836c273f8b43a9950fa7dffa7f3
Positron Robotics <positronrobotics@gmail.com>
2023-04-23 10:32:37 +0100

    indented, and getch and send

--- ci2c.c
+++ ci2c.c
@@ -1,42 +1,63 @@
 #include <linux/i2c-dev.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #define I2C_ADDR 0x08
 #define LED_ON  0xEF
 #define LED_OFF 0xFF
+
+int getch(void)
+{
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldattr );
+	newattr = oldattr;
+	newattr.c_lflag &= ~( ICANON | ECHO );
+	tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+	return ch;
+}
+
+
 int main (void)
 {
-    int value=65;
-    int fd; 
+	int in=0;
+	int value=65;
+	int fd;
 
-	printf("bala:23apr:100\n");
-   
-    fd = open("/dev/i2c-1", O_RDWR);
-	
-printf("bala:23apr:101\n");
+	printf("bala:23apr:101.0\n");
 
-    if (fd < 0) {
-        printf("Error opening file: %s\n", strerror(errno));
-        return 1;
-    }   
-    if (ioctl(fd, I2C_SLAVE, I2C_ADDR) < 0) {
-        printf("ioctl error: %s\n", strerror(errno));
-        return 1;
-    }   
+	fd = open("/dev/i2c-1", O_RDWR);
 
-    while(1)
-    {   
-        if( write( fd , &value, 1 ) != 1) {
-            printf("Error writing file: %s\n", strerror(errno));
-        }    
+	if (fd < 0) {
+		printf("Error opening file: %s\n", strerror(errno));
+		return 1;
+	}   
+	if (ioctl(fd, I2C_SLAVE, I2C_ADDR) < 0) {
+		printf("ioctl error: %s\n", strerror(errno));
+		return 1;
+	}   
 
-	if(value++>=100)
-	{
-		value=65;
+	while(1)
+	{   
+
+		in=getch();
+
+		if( write( fd , &in, 1 ) != 1) {
+			printf("Error writing file: %s\n", strerror(errno));
+		}    
+
+		if(value++>=100)
+		{
+			value=65;
+		}
+
+		usleep(1000000); 
 	}
 
-        usleep(1000000); 
-    }
-    
-    return 0;
+	return 0;
 }
